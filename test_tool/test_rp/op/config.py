fe500cf76906d38f610085cb625b013d27f60da8
daniel evertsson <daniel.evertsson@umu.se>
2016-01-25 11:35:10 +0100

    Added dev doc and usage doc for RP test tool.
    Updated config files

--- test_tool/test_rp/op/config.py
+++ test_tool/test_rp/op/config.py
@@ -11,10 +11,10 @@ keys = [
 ]
 
 multi_keys = [
-    {"type": "RSA", "use": ["enc"], "key": "keys/2nd_enc"},
-    {"type": "RSA", "use": ["sig"], "key": "keys/2nd_sig"},
-    {"type": "RSA", "use": ["enc"], "key": "keys/3rd_enc"},
-    {"type": "RSA", "use": ["sig"], "key": "keys/3rd_sig"},
+    {"type": "RSA", "use": ["enc"], "name": "rsa_enc_1", "key": "keys/pyoidc_enc"},
+    {"type": "RSA", "use": ["sig"], "name": "rsa_sig_1", "key": "keys/pyoidc_sig"},
+    {"type": "RSA", "use": ["enc"], "name": "rsa_enc_2", "key": "keys/sec_enc"},
+    {"type": "RSA", "use": ["sig"], "name": "rsa_sig_2", "key": "keys/sec_sig"},
     {"type": "EC", "crv": "P-256", "use": ["sig"]},
     {"type": "EC", "crv": "P-256", "use": ["enc"]},
     {"type": "EC", "crv": "P-256", "use": ["sig"]},
