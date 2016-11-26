c2413d824c96bca8fd47426eed77bef97c086073
Roland Hedberg <roland.hedberg@adm.umu.se>
2016-11-26 15:39:09 +0100

    Rerwriting the config server

--- test_tool/test_op/configuration_server/config.py
+++ test_tool/test_op/configuration_server/config.py
@@ -1,3 +1,13 @@
+import os
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+SERVER_CERT = "./certs/cert.pem"
+SERVER_KEY = "./certs/key.pem"
+CERT_CHAIN = None
+
+VERIFY_SSL = False
+
 BASE_URL = 'http://localhost'
 TEMPLATE_DIR = './'
 ENT_PATH = 'entities'
