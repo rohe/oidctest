1ac084431b1852d7dcc42e02f31a13aa728ad97c
Roland Hedberg <roland@catalogix.se>
2017-03-24 15:38:36 +0100

    Used to create the test list part of the main page.

---
+++ test_tool/cp/test_rplib/fed_rp/make_test_list.py
@@ -0,0 +1,15 @@
+import json
+
+from oidctest.fed.test_list import test_list, get_mandatory
+
+file_dir = 'flows'
+
+fp = open('links.json', 'r')
+links = json.load(fp)
+fp.close()
+
+grps = [
+    "Single SMS", "Multiple SMS", "Policy Breach", "Wrong Signing Key"
+]
+mandatory = get_mandatory(file_dir, links)
+print("\n".join(test_list(mandatory, grps)))
