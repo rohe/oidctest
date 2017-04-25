59cea8391ebe4d4b7e62035ce80e163bdae0198e
Roland Hedberg <roland@catalogix.se>
2017-04-25 17:07:33 +0200

    Allow for restart of specific instances

--- test_tool/cp/test_op/restart.py
+++ test_tool/cp/test_op/restart.py
@@ -10,6 +10,8 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', dest='test_tool_conf')
+parser.add_argument('-i', dest='iss')
+parser.add_argument('-t', dest='tag')
 parser.add_argument(dest="config")
 args = parser.parse_args()
 
@@ -22,6 +24,14 @@ _app = Application(_conf.TEST_SCRIPT, _conf.FLOWDIR, rest, _conf.PORT_MIN,
                    _conf.PORT_MAX, _ttc.BASE, args.test_tool_conf, '')
 
 for pid, proc_info in find_test_instances('op_test_tool.py').items():
+    if args.iss:
+        if proc_info['iss'] != args.iss:
+            continue
+
+        if args.tag:
+            if proc_info['tag'] != args.tag:
+                continue
+
     print('Restarting: {iss} {tag}'.format(**proc_info))
     ps = psutil.Process(pid)
     ps.kill()
