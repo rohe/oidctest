d800f7cc7ced2a766f736680d1c5b4b4059d1c0e
Roland Hedberg <roland@catalogix.se>
2017-04-26 10:49:50 +0200

    Tool directory

--- src/oidctest/site_setup.py
+++ src/oidctest/site_setup.py
@@ -47,7 +47,7 @@ def oidc_op_setup(distroot):
         shutil.copytree(_src, _dir)
 
     for _fname in ['run_example.sh', 'config_example.py', 'config_server.py',
-                   'tt_config_example.py', 'op_test_tool.py', 'restart.py']:
+                   'tt_config_example.py', 'op_test_tool.py']:
         _file = os.path.join(_op_dir, _fname)
         copy_if_not_same(_file, _fname, True)
 
