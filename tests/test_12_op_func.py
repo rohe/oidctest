2e5f2ee4d855af9c40bc39666ca15a1ead224f59
Roland Hedberg <roland@catalogix.se>
2017-04-30 14:49:14 -0700

    Remove local definition.

--- tests/test_12_op_func.py
+++ tests/test_12_op_func.py
@@ -1,12 +1,10 @@
 import json
 
 from oic import rndstr
-from oic.utils.keyio import build_keyjar, KeyJar
+from oic.utils.keyio import build_keyjar
 from otest.check import ERROR
+from otest.test_setup import setup_conv
 
-from oidctest.op import func
-from oidctest.op import oper
-from oidctest.op.client import Client
 from oidctest.op.func import check_support
 from oidctest.op.func import store_sector_redirect_uris
 from oidctest.op.func import sub_claims
@@ -30,67 +28,17 @@ from oidctest.op.func import id_token_hint
 from oidctest.op.func import set_webfinger_resource
 from oidctest.op.oper import AsyncAuthn
 from oidctest.op.oper import Webfinger
-from oidctest.session import SessionHandler
 
-from otest.aus.handling_ph import WebIh
-from otest.conf_setup import OP_ORDER
-from otest.conversation import Conversation
-from otest.events import Events
 from otest.events import EV_CONDITION
 from otest.events import EV_PROTOCOL_RESPONSE
 from otest.events import EV_RESPONSE
-from otest.flow import FlowState
-from otest.prof_util import ProfileHandler
 
 from oic.oic.message import AccessTokenResponse
-from oic.oic.message import factory
-from oic.oic.message import ProviderConfigurationResponse
-from oic.utils.authn.client import CLIENT_AUTHN_METHOD
 
 KEYDEFS = [
     {"type": "RSA", "key": '', "use": ["sig"]},
     {"type": "EC", "crv": "P-256", "use": ["sig"]}
 ]
-
-KEYJAR = KeyJar()
-
-for iss in ['https://www.swamid.se', 'https://www.sunet.se',
-            'https://www.feide.no', 'https://www.uninett.no']:
-    KEYJAR.import_jwks(build_keyjar(KEYDEFS)[0], iss)
-
-
-def setup_conv():
-    entity = Client(client_authn_method=CLIENT_AUTHN_METHOD,
-                    verify_ssl=False)
-    entity.provider_info = ProviderConfigurationResponse(
-        authorization_endpoint="https://example.com",
-    )
-
-    cls_factories = {'': oper.factory}
-    func_factory = func.factory
-
-    flow_state = FlowState('flows', profile_handler=ProfileHandler,
-                           cls_factories=cls_factories,
-                           func_factory=func_factory,
-                           display_order=OP_ORDER)
-    iss = 'https://example.org'
-    tag = 'foobar'
-    session_handler = SessionHandler(iss, tag,
-                                     flows=flow_state,
-                                     tool_conf={})  # , rest=rest, **webenv)
-    session_handler.iss = iss
-    session_handler.tag = tag
-
-    info = WebIh(session=session_handler, profile_handler=ProfileHandler)
-
-    conv = Conversation([], entity, factory, callback_uris=[])
-    conv.events = Events()
-    conv.tool_config = {}
-    return {'conv': conv, 'io': info}
-
-
-def place_holder(oper, args):
-    pass
 
 
 def test_set_webfinger_resource():
@@ -403,7 +351,6 @@ def test_store_sector_redirect_uris():
     _info = setup_conv()
     oper = AsyncAuthn(_info['conv'], _info['io'], None)
 
-    args = None
     store_sector_redirect_uris(oper, {"other_uris": [
         "https://example.com/op"
     ]})
@@ -438,7 +385,8 @@ def test_sub_claims():
         },
         "token_type": "Bearer"
     }
-    _info['conv'].events.store(EV_PROTOCOL_RESPONSE, AccessTokenResponse(**atr))
+    _info['conv'].events.store(EV_PROTOCOL_RESPONSE,
+                               AccessTokenResponse(**atr))
     _sub = atr["id_token"]["sub"]
     args = None
     sub_claims(oper, args)
