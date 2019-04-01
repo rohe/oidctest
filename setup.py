f0cdc8c3b08c17486534d02ed4b29f4819980c86
roland <roland@catalogix.se>
2019-04-01 19:04:13 +0200

    Added missing HTML (precursor) files

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Copyright (C) 2015 Umea Universitet, Sweden
+# Copyright (C) 2019 Roland Hedberg, Sweden
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
