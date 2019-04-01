8cf8081261b56ccf36d61fca945e116564c15140
roland <roland@catalogix.se>
2019-04-01 10:07:07 +0200

    Use the latest otest version

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
         'future',
         'CherryPy == 8.9.1',
         'oic == 0.15.1',
-        'otest >= 0.7.3',
+        'otest >= 0.7.4',
         #'psutil',
         'cherrypy-cors >= 1.5',
         'six',
